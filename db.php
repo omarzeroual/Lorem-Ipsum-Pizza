a508dbdc0f8b9d619ee9d0b7fe07a2528d15555b
silvanbitterli <silvan@bitterli.org>
2016-09-07 14:16:55 +0200

    Add files via upload

---
+++ db.php
@@ -0,0 +1,26 @@
+<html>
+<body>
+<?php
+
+
+	#### Variablen ####
+	# Rechner, auf dem sich die DB befindet
+	$db_position = 'localhost';
+	$db_datenbank = 'loremipsum-pizza';
+	# Anmeldedaten
+	$db_benutzername  = 'loremipsum-pizza';
+	$db_passwort  = 'insaneinthemainframe';
+	#phpinfo();
+	
+	# Aufbauen der Datenbank Verbindung
+	#$link = mysqli_connect('localhost', 'root', 'root');
+	$link = mysqli_connect($db_position , $db_benutzername , $db_passwort  );
+	
+	if (!$link)
+	{
+		echo "<p> Verbindung fehlgeschlagen</p>";
+	}
+
+?>
+</body>
+</html>
