7c67a06f3cdb613bdfd043a3c21284e2ec8a71b3
silvanbitterli <silvan@bitterli.org>
2016-09-07 18:38:34 +0200

    md5 Hash und Salt

--- db.php
+++ db.php
@@ -10,11 +10,15 @@
 	# Anmeldedaten
 	$db_benutzername  = 'loremipsum-pizza';
 	$db_passwort  = 'insaneinthemainframe';
+    
+    #Variablen zur Datensicherheit
+    $salt = "jhgj56uth";
 	#phpinfo();
 	
 	# Aufbauen der Datenbank Verbindung
 	#$link = mysqli_connect('localhost', 'root', 'root');
-	$link = mysqli_connect($db_position , $db_benutzername , $db_passwort  );
+	$link = mysqli_connect($db_position , $db_benutzername , 'insaneinthemainframe', $db_datenbank  );
+
 	
     #Verbindung konnte nicht aufgebaut werden
 	if (!$link)
@@ -27,10 +31,39 @@
 	{
         
         if (!empty($_POST["einloggen"]))
-            echo "<p>Eingabe getätigt";
-		
+            
+        {
+    
+        $benutzername = $_POST["benutzername"];
+            $passwort = $_POST["passwort"];
+            
+            $passwort = md5($salt.$passwort);
+
+          
+        # SQL Query für die DB-Abfrage
+            
+        $sqlResultat = mysqli_query($link,"SELECT *
+                                           FROM benutzer
+                                           WHERE benutzername = '$benutzername'
+                                             AND salt = '$passwort'" );
+            
+        $sqlAnzahl = mysqli_num_rows($sqlResultat);
         
-	}
+        if ($sqlAnzahl > 0)
+        {
+            echo "<p> User angemeldet";
+        } else {
+            echo "<p> Kein solcher User vorhanden";
+        }
+            
+        }
+    }
+    
+        
+               
+		
+    
+    
 
 ?>
 </body>
